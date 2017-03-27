cb9e551f1f675fcbd1645cd5455951a48b484798
lsw <dididas@naver.com>
2017-03-27 16:54:26 +0900

    dto생성  급한대로 작업은 가능합니다 (sale_detail제외 )

--- src/main/java/kr/or/digt/sw_project/dto/Sale.java
+++ src/main/java/kr/or/digt/sw_project/dto/Sale.java
@@ -1,5 +1,16 @@
 package kr.or.digt.sw_project.dto;
 
-public class Sale {
+import java.util.Date;
 
+public class Sale {
+	private String saleCode;
+	private Client client; 
+	private Software software;
+	private int saleAmount;
+	private boolean isDeposit;
+	private Date orderDate;
+	private int supplyPrice;
+	private int salePrice;
+	private boolean saleIsExist;
+	private SaleDetail sailDetail; 
 }
--- src/main/java/kr/or/digt/sw_project/dto/SupplyCompany.java
+++ src/main/java/kr/or/digt/sw_project/dto/SupplyCompany.java
@@ -1,58 +1,9 @@
 package kr.or.digt.sw_project.dto;
 
 public class SupplyCompany {
-	private String compNo;
+	private String compCode;
 	private String compName;
 	private String compAddr;
 	private String compTel;
-	
-	public SupplyCompany() {}
-
-	public SupplyCompany(String compNo, String compName, String compAddr, String compTel) {
-		super();
-		this.compNo = compNo;
-		this.compName = compName;
-		this.compAddr = compAddr;
-		this.compTel = compTel;
-	}
-
-	public String getCompNo() {
-		return compNo;
-	}
-
-	public void setCompNo(String compNo) {
-		this.compNo = compNo;
-	}
-
-	public String getCompName() {
-		return compName;
-	}
-
-	public void setCompName(String compName) {
-		this.compName = compName;
-	}
-
-	public String getCompAddr() {
-		return compAddr;
-	}
-
-	public void setCompAddr(String compAddr) {
-		this.compAddr = compAddr;
-	}
-
-	public String getCompTel() {
-		return compTel;
-	}
-
-	public void setCompTel(String compTel) {
-		this.compTel = compTel;
-	}
-
-	@Override
-	public String toString() {
-		return String.format("supply_company [compNo=%s, compName=%s, compAddr=%s, compTel=%s]", compNo, compName,
-				compAddr, compTel);
-	}
-	
-	
+	private boolean isExist;
 }
